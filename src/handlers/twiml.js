965416ec9e50d44900881b9d93bea304ac9cc324
Brett Warner <brett@warbrett.com>
2015-12-07 20:36:17 -0700

    lots of cleanup, remove built files

--- src/handlers/twiml.js
+++ src/handlers/twiml.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function twiml (request, reply) {
+  // TODO: Make this a proper template and don't hardcore caller id
   reply(`<Response>
-<Dial callerId="4144090112">${request.query.PhoneNumber}</Dial>
+<Dial callerId="">${request.query.PhoneNumber}</Dial>
 </Response>`).type('text/xml');
 }
 
