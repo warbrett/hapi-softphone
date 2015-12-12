dd8408721054c6a252bc049bbe8afd27f9fb23ce
Brett Warner <brett@warbrett.com>
2015-12-11 20:31:44 -0700

    dynamic caller ids!

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -3,16 +3,24 @@
 import twilio from 'twilio';
 import config from '../config';
 
+const client = new twilio.RestClient(config.twilioAccountSid, config.twilioAuthToken);
 const capability = new twilio.Capability(config.twilioAccountSid, config.twilioAuthToken);
+
 capability.allowClientOutgoing(config.twimlAppSid);
 
 function handler (request, reply) {
   const token = capability.generate();
-  const context = {
-    token
-  };
+  const context = { token };
 
-  reply.view('index', context);
+  client.accounts.incomingPhoneNumbers.list()
+  .then(function ({ incomingPhoneNumbers }) {
+    context.numbers = incomingPhoneNumbers.map(({ friendlyName, phoneNumber }) => {
+      return { friendlyName, phoneNumber };
+    });
+  })
+  .finally(function () {
+    reply.view('index', context);
+  });
 }
 
 export default handler;
--- src/handlers/twiml.js
+++ src/handlers/twiml.js
@@ -2,7 +2,6 @@
 
 function twiml (request, reply) {
   // TODO: Make this a proper template
-  // TODO: Get caller id from front end
   reply(`<Response>
 <Dial callerId="${request.query.CallerId}">${request.query.PhoneNumber}</Dial>
 </Response>`).type('text/xml');
